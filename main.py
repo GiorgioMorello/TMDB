79886c7e8f1c46a60a116a024f6fa0e2afa9f811
Pedro Morello Abbud <abbud07@gmail.com>
2022-08-05 22:15:40 -0300

    feat: styling on index.html

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 import json
-from flask import Flask, jsonify
-from IPython import embed
+from flask import Flask, jsonify, render_template
 from os import getenv
-from models import DiscoverMovieResponse, Movie 
-from typing import List
+from models import DiscoverMovieResponse, Movie, GenreResponse
+from typing import List, Dict
 
 
 TMDB_BASE_API = "https://api.themoviedb.org/3"
@@ -17,11 +16,40 @@ def get_trending_movies(api_key) -> List[Movie]:
     return discover_movie.results
 
 
+def get_genres(api_key) -> Dict[int, str]:
+    genre_route = f"{TMDB_BASE_API}/genre/movie/list?api_key={api_key}"
+    result = requests.get(genre_route)
+    genres_response = GenreResponse(**result.json())
+    return genres_response.to_dict()
+
+def get_genre_color(id: int):
+    colors = [
+        "#f94144",
+        "#f3722c",
+        "#f8961e",
+        "#f9844a",
+        "#f9c74f",
+        "#90be6d",
+        "#43aa8b",
+        "#4d908e",
+        "#577590",
+        "#277da1"
+    ]
+
+    position = id % len(colors)
+    return colors[position]
+
+
 movies = get_trending_movies(API_KEY)
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return jsonify([m for m in movies])
+
+@app.route('/')
+def main():
+    movies = get_trending_movies(API_KEY)
+    genres = get_genres(API_KEY)
+    return render_template('index.html', movies=movies, genres=genres, get_genre_color=get_genre_color)
+
+app.run(debug=True)
--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class OriginalLanguage(Enum):
     EN = "en"
     ES = "es"
 
+TMBD_BASE_PATH = "https://image.tmdb.org/t/p/w500"
 @dataclass
 class Movie:
     adult: bool
@@ -25,7 +26,22 @@ class Movie:
     vote_average: float
     vote_count: int
 
-    def __init__(self, adult: bool, backdrop_path: str, genre_ids: List[int], id: int, original_language: OriginalLanguage, original_title: str, overview: str, popularity: float, poster_path: str, release_date: datetime, title: str, video: bool, vote_average: float, vote_count: int) -> None:
+    def __init__(
+            self,
+            adult: bool,
+            backdrop_path: str,
+            genre_ids: List[int],
+            id: int,
+            original_language: OriginalLanguage,
+            original_title: str,
+            overview: str,
+            popularity: float,
+            poster_path: str,
+            release_date: datetime,
+            title: str,
+            video: bool,
+            vote_average: float,
+            vote_count: int) -> None:
         self.adult = adult
         self.backdrop_path = backdrop_path
         self.genre_ids = genre_ids
@@ -34,7 +50,7 @@ class Movie:
         self.original_title = original_title
         self.overview = overview
         self.popularity = popularity
-        self.poster_path = poster_path
+        self.poster_path = f"{TMBD_BASE_PATH}/{poster_path}"
         self.release_date = release_date
         self.title = title
         self.video = video
@@ -50,6 +66,26 @@ class DiscoverMovieResponse:
 
     def __init__(self, page: int, results: List[Movie], total_pages: int, total_results: int) -> None:
         self.page = page
-        self.results = results
+        self.results = [ Movie(**r) for r in results]
         self.total_pages = total_pages
         self.total_results = total_results
+
+
+@dataclass
+class Genre:
+    id: int
+    name: str
+
+    def __init__(self, id: int, name: str) -> None:
+        self.id = id
+        self.name = name
+
+@dataclass
+class GenreResponse:
+    genres: List[Genre]
+
+    def __init__(self, genres: List[Genre]) -> None:
+        self.genres = [ Genre(**r) for r in genres]
+
+    def to_dict(self):
+        return {  g.id : g.name for g in self.genres }
